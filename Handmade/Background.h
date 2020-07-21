1d49f1101b525898cc6cb59681ae34f562136293
Karsten Vermeulen <djkarstenv@gmail.com>
2020-07-21 14:03:46 +0100

    Made changes to all header file text

--- Handmade/Background.h
+++ Handmade/Background.h
@@ -3,35 +3,28 @@
 
 /*==============================================================================================#
 |                                                                                               |
-| Handmade Lite is an educational game engine, written by Karsten Vermeulen for the purposes of |
-| educating other fellow programmers, programming students and anyone else wishing to learn     |
-| about game development, C++ and OOP. The engine, class design and overall structure is by no  |
-| means perfect and there is certainly room for improvement. Feel free to use, copy, break,     |
-| update and do as you wish with this code - it is there, free, for all!                        |
-|																							    |
+| Handmade Lite is an educational Visual Studio framework, written by Karsten Vermeulen,        |
+| designed to assist with educating other fellow programmers, programming students, and anyone  |
+| else wishing to learn about game development, C++, and object-oriented programming. This      |
+| project is not open source and therefore has no license affiliated with it. Please be         |
+| respectful and do not distribute the engine or its code as your work. You may alter or tweak  |
+| the engine code as a learning experience and you may also 'borrow' code and functionality     |
+| and use it to create your own game engine. Finally, please do use Handmade Lite to develop    |
+| a game.                                                                                       |
+|                                                                                               |
 | Designed to teach. Made from scratch. Built by hand.							                |
-|																							    |
-#===============================================================================================#
-|	                                                                                            |
-| Please note, that this project is not open source and therefore has no license affiliated     |
-| with it. However, feel free to use the engine to build your own game or learn from the code.  |
-| Handmade Lite is an educational tool, designed to be used as such. Please be respectful and   |
-| do not distribute the engine or its code as your own work. You may alter or tweak the engine  |
-| code as a learning experience and you may also 'borrow' some code and functionality and use   |
-| it to create your own game engine or game.                                                    |
 |	                                                                                            |
 #===============================================================================================#
 |                                                                                               |
 | If you like Handmade Lite and wish to show your support, if you have any questions about      |
 | the project, or if you just want to reach out, please do get in touch:                        |
 |																						        |
-| Facebook: https://www.facebook.com/KarstensCorner								                |
-| Twitter: https://twitter.com/KarstensCorner													|
-| LinkedIn: https://www.linkedin.com/in/karstenvermeulen                                        |
-| GitHub: https://github.com/djkarstenv									                        |
+| Facebook | https://www.facebook.com/Handmade-Lite-107188757745777								|
+| Twitter  | https://twitter.com/KarstensCorner													|
+| GitHub   | https://github.com/djkarstenv/Handmade-Lite									    |
 |                                                                                               |
 #===============================================================================================#
-| 'Background' source files last updated in June 2020						                    |
+| 'Background' source files last updated on 21 July 2020							            |
 #==============================================================================================*/
 
 #include "GameObject.h"
