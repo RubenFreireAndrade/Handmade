97c3d067aeb0a03aef2c03e7b62b5a04ea29ee73
Karsten Vermeulen <karsten.vermeulen.dev@gmail.com>
2022-03-11 14:36:34 +0000

    Add dynamic collision checks based on time and calc. collision region

--- Handmade/AABB.h
+++ Handmade/AABB.h
@@ -6,11 +6,20 @@ class AABB
 
 public:
 
+	AABB GetCollisionRegion(const AABB& secondBox);
+
 	void SetPosition(int x, int y);
 	void SetDimension(int width, int height);
 
 	void Update();
 	bool IsColliding(const AABB& secondBox) const;
+
+	bool IsColliding(
+		const AABB& secondBox,
+		const Vector<int>& lhsBoxVelocity, // velocity of rectangle 1
+		const Vector<int>& rhsBoxVelocity, // velocity of rectangle 2
+		float maxTime, // max time - i.e. your DT for this frame
+		float* firstTime = 0); // Set to time of first contact 
 
 private:
 
